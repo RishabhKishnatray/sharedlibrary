870e5a7da4faad8fded2b358d33ecd385ff943c9
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-09-06 17:15:00 +0530

    Delete giturlWorld.groovy

--- giturlWorld.groovy
+++
@@ -1,7 +0,0 @@
-def call(String gitUrl, String credentialsId, String branch = 'master') {
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name: branch]],
-        userRemoteConfigs: [[url:https://github.com/ritesh107/common-shared-library.git]]
-    ])
-}
