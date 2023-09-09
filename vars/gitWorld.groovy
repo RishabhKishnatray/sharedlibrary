dbefa1f7f8faece3f0487b9ce5204da40e559f36
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-09-09 23:19:00 +0530

    Update gitWorld.groovy

--- vars/gitWorld.groovy
+++ vars/gitWorld.groovy
@@ -2,6 +2,6 @@ def call(String gitUrl, String branch = 'main') {
     checkout([
         $class: 'GitSCM',
         branches: [[name: main]],
-        userRemoteConfigs: [[[url: https://github.com/ritesh107/common-shared-library.git]]
+        userRemoteConfigs: [[[url:]]
     ])
 }
