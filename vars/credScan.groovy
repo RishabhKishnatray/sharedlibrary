29f2bf5b812e5b29ed8ee5e62ef952cf7553aa67
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-09-09 19:35:53 +0530

    Create credScan.groovy

---
+++ vars/credScan.groovy
@@ -0,0 +1,40 @@
+def call() {
+    pipeline {
+        agent any
+        tools {
+            git 'Default'
+            jdk 'java'
+        }
+        stages {
+            stage('clean workspace') {
+                steps {
+                    cleanWs()
+                }
+            }
+            stage('git clone') {
+                steps {
+                    script {
+                        sh 'git clone https://github.com/RishabhKishnatray/employee-api.git'
+                    }
+                }
+            }
+            stage('gitleaks') {
+                steps {
+                    script {
+                        sh 'wget https://github.com/zricethezav/gitleaks/releases/download/v7.0.2/gitleaks-linux-amd64 -O gitleaks'
+                        sh 'chmod +x gitleaks'
+                        sh 'sudo cp gitleaks /usr/local/bin/'
+                    }
+                }
+            }
+            stage('run') {
+                steps {
+                    sh '''
+                    cd employee-api
+                    gitleaks detect -p . -v
+                    '''
+                }
+            }
+        }
+    }
+}
