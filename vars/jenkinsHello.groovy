301f759bf94c357670a325428ef51e5dc078e3f7
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-09-16 18:14:39 +0530

    Update jenkinsHello.groovy

--- vars/jenkinsHello.groovy
+++ vars/jenkinsHello.groovy
@@ -1,40 +1,44 @@
-def call() {
-pipeline {
-    agent any
-    tools {
-        git 'Default'
-        jdk 'java'
-    }
+def call(name) {
+echo "hey${name}, how are you?"
+}
 
-    stages {
-        stage('clean workspace') {
-            steps {
-                cleanWs()
-            }
-        }
-        stage('git clone') {
-            steps {
-                sh 'git clone https://github.com/RishabhKishnatray/employee-api.git'
-            }
-        }
-        stage('gitleaks') {
-            steps {
-            script {
-                    sh 'wget https://github.com/zricethezav/gitleaks/releases/download/v7.0.2/gitleaks-linux-amd64 -O gitleaks'
-                    sh 'chmod +x gitleaks'
-                    sh 'sudo cp gitleaks /usr/local/bin/'
-                }
-            }
-        }
-            stage('run') {
-            steps {
-                sh '''
-                cd employee-api
-                 gitleaks detect -p . -v
-                 '''
-                }
-            }
-        }
-    }
-}    
+// def call() {
+// pipeline {
+//     agent any
+//     tools {
+//         git 'Default'
+//         jdk 'java'
+//     }
+
+//     stages {
+//         stage('clean workspace') {
+//             steps {
+//                 cleanWs()
+//             }
+//         }
+//         stage('git clone') {
+//             steps {
+//                 sh 'git clone https://github.com/RishabhKishnatray/employee-api.git'
+//             }
+//         }
+//         stage('gitleaks') {
+//             steps {
+//             script {
+//                     sh 'wget https://github.com/zricethezav/gitleaks/releases/download/v7.0.2/gitleaks-linux-amd64 -O gitleaks'
+//                     sh 'chmod +x gitleaks'
+//                     sh 'sudo cp gitleaks /usr/local/bin/'
+//                 }
+//             }
+//         }
+//             stage('run') {
+//             steps {
+//                 sh '''
+//                 cd employee-api
+//                  gitleaks detect -p . -v
+//                  '''
+//                 }
+//             }
+//         }
+//     }
+// }    
 
