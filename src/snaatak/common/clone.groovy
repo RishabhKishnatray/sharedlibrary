891865654db14c18e1d82aee0aaf298a672b35d6
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-09-18 01:18:54 +0530

    Update clone.groovy

--- src/snaatak/common/clone.groovy
+++ src/snaatak/common/clone.groovy
@@ -1,6 +1,6 @@
 package snaatak.common
 
-def call(String repoUrl = 'https://github.com/RishabhKishnatray/attendance-api.git', String credentialsId = 'shared-library', String branchName = 'main') {
+def call(String repoUrl = 'https://github.com/RishabhKishnatray/attendance-api.git', String credentialsId = 'shared', String branchName = 'main') {
        stage('Cloning Repo') {
         git branch: branchName, credentialsId: credentialsId, url: repoUrl
     }
