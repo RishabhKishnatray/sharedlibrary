60bc872e69996d7caee201b062362c30c44cec0e
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-09-17 16:28:14 +0530

    Update temp.groovy

--- src/snaatak/template/temp.groovy
+++ src/snaatak/template/temp.groovy
@@ -1,1 +1,7 @@
+package snaatak.template
+import snaatak.common.*
+def call(String repoUrl, String branchName, String credentialId){
+  Clone = new Clone()
 
+  Clone.call(repoUrl, branchName, credentialId)
+}
